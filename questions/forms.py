7041fc11586b0dd8181781713a77df0078c9c492
Chris <chris@fry-it.com>
2011-11-28 07:48:03 +0800

    change to add news function

--- questions/forms.py
+++ questions/forms.py
@@ -1,8 +1,8 @@
 from django import forms
  
 class AddQuestionForm(forms.Form):
-    question = forms.CharField(max_length=200)
-    description = forms.CharField(widget=forms.Textarea, required=False)
+    question = forms.CharField(max_length=200, label="* Question title")
+    description = forms.CharField(widget=forms.Textarea, required=False, label="Extra information/details")
 
     
 class AddAnswerForm(forms.Form):
