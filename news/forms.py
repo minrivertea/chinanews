88271fc4cc7879450b79fdf0b6a004497dc5c3b2
Chris <chris@fry-it.com>
2011-11-28 14:58:56 +0800

    clean_url method

--- news/forms.py
+++ news/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.forms import ModelForm
 from django.contrib.auth.models import User
+import re
+from urlparse import urlparse, urljoin
 
 
 from news.models import *
@@ -12,3 +14,12 @@ class AddNewsForm(forms.Form):
         help_text="Use this if you're posting a link")
     text = forms.CharField(widget=forms.Textarea, required=False, label='Text',
         help_text="Use this if you're starting a discussion")
+    
+    
+    def clean_url(self):
+        # we just want to check that the URL is properly formed
+        data = urlparse(self.cleaned_data['url'])
+        if not data.scheme:
+            data = "http://%s" % self.cleaned_data['url']
+                
+        return data
